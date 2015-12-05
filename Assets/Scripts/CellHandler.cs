0384130b17db081d9fdadd5d7b445ad418e3ffc1
Ionut Mocanu <ionut.mocanu@erepubliklabs.com>
2015-12-05 23:51:54 +0200

    - Work on lights out algorithm

---
+++ Assets/Scripts/CellHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CellHandler : MonoBehaviour {
+
+    public void OnClick()
+    {
+        var child = transform.FindChild("Collider");
+        child.gameObject.SetActive(true);
+        SwitchCellColor();
+        StartCoroutine("DeactivateCellCollider");
+    }
+
+    public void OnMouseOver()
+    {
+        Debug.LogError(gameObject.name);
+    }
+
+    public void SwitchCellColor()
+    {
+        if (gameObject.GetComponent<Image>().color == GameManager.Instance.cellOffColor)
+        {
+            gameObject.GetComponent<Image>().color = GameManager.Instance.cellOnColor;
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().color = GameManager.Instance.cellOffColor;
+        }
+    }
+
+    IEnumerator DeactivateCellCollider()
+    {
+        yield return new WaitForSeconds(0.1f);
+        var child = transform.FindChild("Collider");
+        child.gameObject.SetActive(false);
+        gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public ColliderHandler colliderHandler;
+    public CellHandler cellHandler;
+
+    public GameObject grid;
+    public GameObject cell;
+
+    public Color cellOffColor = new Color(0.53f, 0.8f, 0.77f);
+    public Color cellOnColor = Color.yellow;
+
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void OnEnable()
+    {
+        cell.gameObject.GetComponent<Image>().color = cellOffColor;
+
+        var gridSize = 25;
+
+        for(var i=1; i<= gridSize; i++) {
+            GameObject cellClone = (GameObject)Instantiate(cell, new Vector3(1,1, 1), Quaternion.identity);
+            cellClone.transform.SetParent(grid.transform);
+            cellClone.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
